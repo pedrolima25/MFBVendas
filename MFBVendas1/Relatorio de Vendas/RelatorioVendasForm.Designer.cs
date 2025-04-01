f2038db2582502d73a137ea79b2b6f1cc5f77459
alessandro <sdpedrolima@gmai.com>
2025-04-01 11:37:20 -0400

    Melhorias no sistema

--- MFBVendas1/Relatorio de Vendas/RelatorioVendasForm.Designer.cs
+++ MFBVendas1/Relatorio de Vendas/RelatorioVendasForm.Designer.cs
@@ -24,6 +24,8 @@
 
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RelatorioVendasForm));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.lblDataInicio = new System.Windows.Forms.Label();
             this.dtpDataInicio = new System.Windows.Forms.DateTimePicker();
             this.lblDataFim = new System.Windows.Forms.Label();
@@ -39,52 +41,65 @@
             // lblDataInicio
             // 
             this.lblDataInicio.AutoSize = true;
-            this.lblDataInicio.Location = new System.Drawing.Point(12, 15);
+            this.lblDataInicio.Location = new System.Drawing.Point(16, 18);
+            this.lblDataInicio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDataInicio.Name = "lblDataInicio";
-            this.lblDataInicio.Size = new System.Drawing.Size(60, 13);
+            this.lblDataInicio.Size = new System.Drawing.Size(70, 16);
             this.lblDataInicio.TabIndex = 0;
             this.lblDataInicio.Text = "Data Início";
             // 
             // dtpDataInicio
             // 
-            this.dtpDataInicio.Location = new System.Drawing.Point(76, 12);
+            this.dtpDataInicio.Location = new System.Drawing.Point(101, 15);
+            this.dtpDataInicio.Margin = new System.Windows.Forms.Padding(4);
             this.dtpDataInicio.Name = "dtpDataInicio";
-            this.dtpDataInicio.Size = new System.Drawing.Size(200, 20);
+            this.dtpDataInicio.Size = new System.Drawing.Size(265, 22);
             this.dtpDataInicio.TabIndex = 1;
             // 
             // lblDataFim
             // 
             this.lblDataFim.AutoSize = true;
-            this.lblDataFim.Location = new System.Drawing.Point(282, 15);
+            this.lblDataFim.Location = new System.Drawing.Point(376, 18);
+            this.lblDataFim.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDataFim.Name = "lblDataFim";
-            this.lblDataFim.Size = new System.Drawing.Size(49, 13);
+            this.lblDataFim.Size = new System.Drawing.Size(61, 16);
             this.lblDataFim.TabIndex = 2;
             this.lblDataFim.Text = "Data Fim";
             // 
             // dtpDataFim
             // 
-            this.dtpDataFim.Location = new System.Drawing.Point(339, 12);
+            this.dtpDataFim.Location = new System.Drawing.Point(452, 15);
+            this.dtpDataFim.Margin = new System.Windows.Forms.Padding(4);
             this.dtpDataFim.Name = "dtpDataFim";
-            this.dtpDataFim.Size = new System.Drawing.Size(200, 20);
+            this.dtpDataFim.Size = new System.Drawing.Size(265, 22);
             this.dtpDataFim.TabIndex = 3;
             // 
             // btnGerarRelatorio
             // 
-            this.btnGerarRelatorio.Location = new System.Drawing.Point(545, 10);
+            this.btnGerarRelatorio.Image = ((System.Drawing.Image)(resources.GetObject("btnGerarRelatorio.Image")));
+            this.btnGerarRelatorio.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnGerarRelatorio.Location = new System.Drawing.Point(595, 596);
+            this.btnGerarRelatorio.Margin = new System.Windows.Forms.Padding(4);
             this.btnGerarRelatorio.Name = "btnGerarRelatorio";
-            this.btnGerarRelatorio.Size = new System.Drawing.Size(100, 23);
+            this.btnGerarRelatorio.Size = new System.Drawing.Size(122, 51);
             this.btnGerarRelatorio.TabIndex = 4;
             this.btnGerarRelatorio.Text = "Gerar Relatório";
+            this.btnGerarRelatorio.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGerarRelatorio.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnGerarRelatorio.UseVisualStyleBackColor = true;
             this.btnGerarRelatorio.Click += new System.EventHandler(this.btnGerarRelatorio_Click);
             // 
             // dataGridViewRelatorio
             // 
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridViewRelatorio.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridViewRelatorio.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewRelatorio.Location = new System.Drawing.Point(15, 68);
+            this.dataGridViewRelatorio.Location = new System.Drawing.Point(19, 84);
+            this.dataGridViewRelatorio.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridViewRelatorio.Name = "dataGridViewRelatorio";
-            this.dataGridViewRelatorio.Size = new System.Drawing.Size(630, 250);
+            this.dataGridViewRelatorio.Size = new System.Drawing.Size(698, 492);
             this.dataGridViewRelatorio.TabIndex = 5;
+            this.dataGridViewRelatorio.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewRelatorio_CellContentClick);
             // 
             // cmbTipoRelatorio
             // 
@@ -93,35 +108,43 @@
             this.cmbTipoRelatorio.Items.AddRange(new object[] {
             "Produtos Vendidos",
             "Produtos Vendidos por Cliente"});
-            this.cmbTipoRelatorio.Location = new System.Drawing.Point(91, 38);
+            this.cmbTipoRelatorio.Location = new System.Drawing.Point(121, 47);
+            this.cmbTipoRelatorio.Margin = new System.Windows.Forms.Padding(4);
             this.cmbTipoRelatorio.Name = "cmbTipoRelatorio";
-            this.cmbTipoRelatorio.Size = new System.Drawing.Size(200, 21);
+            this.cmbTipoRelatorio.Size = new System.Drawing.Size(265, 24);
             this.cmbTipoRelatorio.TabIndex = 6;
             // 
             // lblTipoRelatorio
             // 
             this.lblTipoRelatorio.AutoSize = true;
-            this.lblTipoRelatorio.Location = new System.Drawing.Point(12, 41);
+            this.lblTipoRelatorio.Location = new System.Drawing.Point(16, 50);
+            this.lblTipoRelatorio.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTipoRelatorio.Name = "lblTipoRelatorio";
-            this.lblTipoRelatorio.Size = new System.Drawing.Size(73, 13);
+            this.lblTipoRelatorio.Size = new System.Drawing.Size(93, 16);
             this.lblTipoRelatorio.TabIndex = 7;
             this.lblTipoRelatorio.Text = "Tipo Relatório";
             // 
             // btnExportar
             // 
-            this.btnExportar.Location = new System.Drawing.Point(545, 38);
+            this.btnExportar.Image = ((System.Drawing.Image)(resources.GetObject("btnExportar.Image")));
+            this.btnExportar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnExportar.Location = new System.Drawing.Point(9, 596);
+            this.btnExportar.Margin = new System.Windows.Forms.Padding(4);
             this.btnExportar.Name = "btnExportar";
-            this.btnExportar.Size = new System.Drawing.Size(100, 23);
+            this.btnExportar.Size = new System.Drawing.Size(122, 51);
             this.btnExportar.TabIndex = 8;
+            this.btnExportar.TabStop = false;
             this.btnExportar.Text = "Exportar Excel";
+            this.btnExportar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnExportar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnExportar.UseVisualStyleBackColor = true;
             this.btnExportar.Click += new System.EventHandler(this.btnExportar_Click);
             // 
             // RelatorioVendasForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(664, 341);
+            this.ClientSize = new System.Drawing.Size(741, 690);
             this.Controls.Add(this.btnExportar);
             this.Controls.Add(this.lblTipoRelatorio);
             this.Controls.Add(this.cmbTipoRelatorio);
@@ -131,9 +154,12 @@
             this.Controls.Add(this.lblDataFim);
             this.Controls.Add(this.dtpDataInicio);
             this.Controls.Add(this.lblDataInicio);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "RelatorioVendasForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Relatório de Vendas";
+            this.Load += new System.EventHandler(this.RelatorioVendasForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewRelatorio)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
